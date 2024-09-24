d28e41823688540e078cf447ee023b4bc021272f
GabiIuliia <oldbobby26@inbox.ru>
2024-09-24 10:36:56 +0300

    что-то

---
+++ pyton5.py
@@ -0,0 +1,9 @@
+# Управляющие последовательности в строках
+# Escape sequence
+from unittest.mock import patch
+
+string= 'Привет\n\tучастникам \nсоревнований'
+path= 'C:\\Windows\\com.exe'
+
+print(string)
+print(path)
---
+++ zadacha.py
@@ -0,0 +1,6 @@
+batton = input('Цена булочки?')
+tea = input('Цена напитка?')
+
+total = int(batton) + int(tea)
+
+print('С Вас', total, 'руб.')
