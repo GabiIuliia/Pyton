5a5ec2c27ea590f6032bd7318916ad382149f8da
GabiIuliia <oldbobby26@inbox.ru>
2024-09-26 09:55:00 +0300

    задача на флаг

--- flag2.py
+++ flag2.py
@@ -1,13 +1,16 @@
-# Цикличное выполнение программы
-from random import choice
+#
 
 choice = ''  # инициализация выбора пользователя
-
-while choice != 'Q':
+work = True
+while work:
     choice = input('Введите R чтобы повернуть направо или L чтобы повернуть налево:')
     if choice == 'L' or choice == 'l':
         print('Вы повернули налево', choice)
     if choice == 'R' or choice == 'r':
-            print('Вы повернули направо', choice)
-    else: print('Вы продолжили движение прямо', choice)
-else: print('Всего доброго')
+        print('Вы повернули направо', choice)
+    if choice == 'Q':
+        work = not work  # инвертировали значение
+    else:
+        print('Вы продолжили движение прямо', choice)
+else:
+    print('Всего доброго')
---
+++ for.py
@@ -0,0 +1,8 @@
+# Введение в цикл For
+# s- start (по умолчанию 0)
+# s- stop (не включая)
+# s- step (шаг- по умолчанию 1)
+# range
+
+for x in range(3, 31, 3)
+    print(x)
