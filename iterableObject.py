5536995aba6cbd308e72f82dfe5ece25ff83595e
GabiIuliia <oldbobby26@inbox.ru>
2024-09-26 11:54:30 +0300

    задача на печать числе с окончанием на определенное число

---
+++ iterableObject.py
@@ -0,0 +1,13 @@
+# Итерируемые объекты iterable object
+# int float не я вляются итерируемыми объектами
+
+
+string= 'абвгдейка'
+
+for symbol in string:
+    print(symbol)
+
+string2='росток'
+
+if 'ток' in string2:
+    print('ток есть в строке', string2)
